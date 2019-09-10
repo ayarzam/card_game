ac7d07b51a8d4ead55b6246a155b7783134f8abe
Ayarza Manwaring <ayarza.manwaring@gmail.com>
2019-09-10 19:53:56 -0400

    adding clause for poker game still need more research on the best way to add these in

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const rp = require('request-promise')
 const app = express()
 
 app.get('/', async function (req, res, next){
+  let holder = []
   console.log('getting all cards')
   let options = {
     url: 'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1',
@@ -19,6 +20,24 @@ app.get('/', async function (req, res, next){
     }
     let drawResponse = await rp(drawOptions)
     console.log(drawResponse)
+    drawResponse.cards.map(card => {
+      holder.push({
+        value: card.value,
+        suit: card.suit
+      })
+    })
+    if (holder[0].suit === holder[1].suit === holder[2].suit === holder[3].suit === holder[4].suit){
+      console.log('flush')
+    // } else if(){
+
+    } else if (holder[0].value !== holder[1].value !== holder[2].value !== holder[3].value !== holder[4].value){
+      console.log('No Pair')
+    }
+    // console.log(holder[0].value, holder[0].suit)
+    // console.log(holder[1].value, holder[1].suit)
+    // console.log(holder[2].value, holder[2].suit)
+    // console.log(holder[3].value, holder[3].suit)
+    // console.log(holder[4].value, holder[4].suit)
   } catch (error) {
     console.log('there is a problem getting all cards', error)
   }
