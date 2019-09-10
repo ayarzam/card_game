27aa719f500ef41af7502f21362ec5b01717816a
Ayarza Manwaring <ayarza.manwaring@gmail.com>
2019-09-10 15:21:31 -0400

    initial commit setting up express server with simple response sending and simple console logging

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res, next){
+  console.log('testing server set up')
+  res.send('Hello World')
+});
+
+app.listen(3000, function(){
+  console.log('Card app listening on port 3000')
+})
