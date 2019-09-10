53dd982960690d538e37ea34ebb15e62dcec0932
Ayarza Manwaring <ayarza.manwaring@gmail.com>
2019-09-10 16:19:41 -0400

    initial request for a new deck

--- app.js
+++ app.js
@@ -3,9 +3,19 @@ const request = require('request')
 const rp = require('request-promise')
 const app = express()
 
-app.get('/', function (req, res, next){
-  console.log('testing server set up')
-  res.send('Hello World')
+app.get('/', async function (req, res, next){
+  console.log('getting all cards')
+  let options = {
+    url: 'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1',
+    json: true
+  }
+  try {
+    let response = await rp(options)
+    let deckId = response.deck_id
+    console.log(deckId)
+  } catch (error) {
+    console.log('there is a problem getting all cards', error)
+  }
 });
 
 app.listen(3000, function(){
